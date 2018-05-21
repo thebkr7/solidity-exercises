ca2c62616186badf46168e898f04eec9407aac70
toledoal <alex@Alejandros-MacBook-Pro.local>
2018-05-21 13:25:28 -0500

    Iterable

--- exercises/iterable-mapping/contracts/Airdrop.sol
+++ exercises/iterable-mapping/contracts/Airdrop.sol
@@ -8,13 +8,33 @@ contract Airdrop {
     // implement addAddress(address) that adds address to the addresses array
     // make a precondition that address is not a duplicate
     function addAddress(address _address) public {
-        require(isSet[_address]);
+        require(isSet[_address] == false);
+        isSet[_address] = true;
+        balances[_address] = 0;
         addresses.push(_address);
     }
 
     // implement removeAddress(address) that removes address from the addresses array
     // make a precondition that address is already set
-    function removeAddress
+    function removeAddress(address _address) public {
+        require(isSet[_address] == true);
+        isSet[_address] = false;
+        delete balances[_address];
+        for (uint i = 0; i<addresses.length; i++) {
+            if (addresses[i] == _address) {
+                delete addresses[i];
+                break;
+             }
+        }
+    }
 
-    // implement airdrop(uint value) that increase balance of each address by specified value
+    // implement airdrop(uint value) that increase 
+    //balance of each address by specified value
+    function airdrop(uint value) public {
+        for (uint i = 0; i < addresses.length; i++) {
+            if (isSet[addresses[i]]){
+                balances[addresses[i]] += value;
+            }
+        }
+    }
 }
