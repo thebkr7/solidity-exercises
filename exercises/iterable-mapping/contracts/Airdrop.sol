91b7f652b6a16106d07726702e46408495111022
toledoal <alex@Alejandros-MacBook-Pro.local>
2018-05-21 12:03:24 -0500

    Iterable

--- exercises/iterable-mapping/contracts/Airdrop.sol
+++ exercises/iterable-mapping/contracts/Airdrop.sol
@@ -7,9 +7,14 @@ contract Airdrop {
 
     // implement addAddress(address) that adds address to the addresses array
     // make a precondition that address is not a duplicate
+    function addAddress(address _address) public {
+        require(isSet[_address]);
+        addresses.push(_address);
+    }
 
     // implement removeAddress(address) that removes address from the addresses array
     // make a precondition that address is already set
+    function removeAddress
 
     // implement airdrop(uint value) that increase balance of each address by specified value
 }
--- exercises/mapping/contracts/Team.sol
+++ exercises/mapping/contracts/Team.sol
@@ -1,4 +1,5 @@
 pragma solidity 0.4.23;
+// pragma experimantal ABIEncoderV2;
 
 contract Team {
     struct Member {
@@ -6,10 +7,20 @@ contract Team {
     }
 
     // create public mapping that maps member id (uint) to a Member
+    mapping (uint => Member) public members;
 
     // create setMember for adding a new member that accepts _id, and _name as arguments
+    function setMember(uint _id, string _name) public {
+        members[_id] = Member(_name);
+    }
 
     // create getMemberName that returns name of the member by id
+    function getMemberName(uint _id) public view returns (string) {
+        return members[_id].name;
+    }
 
     // create removeMember that "removes" member by id
+    function removeMember(uint _id) public {
+        delete members[_id];
+    }
 }
