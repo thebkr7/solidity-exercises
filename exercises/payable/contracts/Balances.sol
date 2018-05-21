8d489ca856068445aabd5995df1fe064597f5310
toledoal <alex@Alejandros-MacBook-Pro.local>
2018-05-21 14:09:45 -0500

    Payable

--- exercises/payable/contracts/Balances.sol
+++ exercises/payable/contracts/Balances.sol
@@ -5,4 +5,9 @@ contract Balances {
     mapping(address => uint) public balances;
 
     // implement deposit() function and track users balances, msg.value has to be greater than 1 finney
+    function deposit() public payable {
+        require(msg.value > 1 finney && msg.value != 0);
+        balances[msg.sender] += msg.value;
+    
+    }
 }
