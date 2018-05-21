51a66eda8498b3295f9a761bc6884b75d7f79449
toledoal <alex@Alejandros-MacBook-Pro.local>
2018-05-21 11:25:26 -0500

    Second batch

--- exercises/struct/contracts/Team.sol
+++ exercises/struct/contracts/Team.sol
@@ -2,10 +2,23 @@ pragma solidity 0.4.23;
 
 contract Team {
     // define a Manager type as struct with name (string) and score (uint)
-
+    struct Manager {
+        string name;
+        uint score;
+    }
     // declare manager variable of type Manager
+    Manager manager;
 
-    // initialize manager witin public setManager function that accepts name and score as arguments
+    // initialize manager writin public setManager function 
+    //that accepts name and score as arguments
+    function setManager(string _name, uint _score) public {
+        manager = Manager(_name, _score);
+    }
 
-    // create increaseManagerScore that accepts number of scores to add to manager.score
+    // create increaseManagerScore that accepts number of scores to 
+    //add to manager.score
+    function increaseManagerScore(uint _score) public {
+        manager.score = manager.score + _score;
+    }
+
 }
