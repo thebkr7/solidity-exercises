84cbb99ad98c8732cfa85c202f18ed968adc2bdc
toledoal <alex@Alejandros-MacBook-Pro.local>
2018-05-21 13:36:04 -0500

    Returning Values

--- exercises/returning-values/contracts/Bookshelf.sol
+++ exercises/returning-values/contracts/Bookshelf.sol
@@ -15,9 +15,12 @@ contract Bookshelf {
 
     function getBook(uint _id) view public returns (string, uint) {
         // look up the book by id and return title and pages using single return instruction
+        return (books[_id].title, books[_id].pages);
     }
 
     function getBookTitle(uint _id) view public returns (string title) {
         // use getBook and return title without using return explicitly and in a single line
+        uint256 pages;
+        (title, pages) = getBook(_id);
     }
 }
